70df92fff4c608ff7208d021be4a76c86ddb6443
Kkushalkumar <22kushal2004@gmail.com>
2026-05-14 16:12:29 +0530

    Second commit

--- phase-1/app.py
+++ phase-1/app.py
@@ -430,7 +430,7 @@ with tab2:
 st.markdown("""
 <hr>
 <div style='text-align: center; color: #888; font-size: 0.9em;'>
-    &copy; 2026 Healthbot | Powered by Groq LLM & SerpAPI | <a href='https://github.com/Kkushalkumar' target='_blank'>GitHub</a>
+    &copy; 2026 Healthbot | Powered by Groq LLM & SerpAPI | <a href='https://github.com/Kkushalkumar/Healthchatbot' target='_blank'>GitHub</a>
 </div>
 """, unsafe_allow_html=True)
 
